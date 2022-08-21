b6722a15e13c2483daeb5fa8d2c644336579f8e5
Egor <pro100egar@gmail.com>
2022-08-21 14:58:20 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int[] numbers = {};
+        // int number1 = Convert.ToInt32(Console.ReadLine());
+        // int div = 100;
+        // int i = 0;
+
+        // while(i < 3)
+        // {
+        //     numbers[i] = number1/div;
+        //     div = div/10;
+        //     Console.WriteLine(numbers[i]);
+        //     i++;
+        // }
+        // //Console.WriteLine(numbers[^2]); почему-то не сработало :(
+        Console.Write("Введите трехзначное число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        number1 = number1 % 100 / 10;
+        Console.WriteLine(number1);
+        
+
+
+    }
+}
