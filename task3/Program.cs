c4e833836d33665594729564df8e3a625e9edec0
Egor <pro100egar@gmail.com>
2022-08-21 14:58:44 +0700

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {     
+        Console.WriteLine("Введите цифру обозначающую день недели: ");
+        Console.WriteLine("1 - Пн, 2 - Вт, 3 - Ср, 4 - Чт, 5 - Пт, 6 - Сб, 7 - Вс ");
+        int numWeekDay = Convert.ToInt32(Console.ReadLine());
+        if (numWeekDay > 5)
+        {
+            Console.WriteLine("Сегодня выходной");
+        }
+        else
+        {
+            Console.WriteLine("Сегодня будний день");
+        }
+    }
+}
