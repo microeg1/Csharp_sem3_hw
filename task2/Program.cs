301568bb9fcf05374b1d4ed0604d9c84550ea876
Egor <pro100egar@gmail.com>
2022-08-21 14:58:34 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {     
+        Console.Write("Введите число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        if (number1 < -100 && number1 > 100)
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+        else
+        {
+            if (number1<0)
+            {
+                number1 = number1 * -1;
+            }
+            string thrdNum = Convert.ToString(number1);
+            Console.WriteLine(thrdNum[2]);
+        }
+    }
+}
